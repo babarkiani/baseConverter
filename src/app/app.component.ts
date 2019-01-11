0ce8a6838b14179213afd63223d0f2a71c160d21
Babar kk <babar>
2019-01-11 15:56:06 +0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
-
+import {FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'base-converter';
+  form;
+
+  ngOnInit(){
+  this.form  = new FormGroup ({
+      decimal: new FormControl(""),
+      binary: new FormControl(""),
+      octal: new FormControl(""),
+      hexa: new FormControl("")
+    });
+  }
+
+  decimalChanged = function(oldValue,newValue){
+    if(newValue != ""){
+      this.form.patchValue({binary: parseInt(newValue, 10).toString(2)});
+      this.form.patchValue({octal: parseInt(newValue, 10).toString(8)});
+      this.form.patchValue({hexa: parseInt(newValue, 10).toString(16)});
+    }
+    else{
+      this.form.patchValue({binary: ""});
+      this.form.patchValue({octal: ""});
+      this.form.patchValue({hexa: ""});
+    }
+
+
+  }
+  b=0;
+  o=0;
+  h=0;
+
+  binaryChanged = function(oldValue,newValue){
+    this.b= this.b +1;
+    if(this.b ==1){
+      if(newValue != ""){
+        this.form.patchValue({decimal: parseInt(newValue, 2).toString(10)});
+      }
+      else{
+        this.form.patchValue({decimal: ""});
+      }
+      this.b=0;
+    }
+   
+  
+
+  }
+
+  octalChanged = function(oldValue,newValue){
+    this.o = this.o +1;
+      if(this.o == 1){
+        if(newValue != ""){
+          this.form.patchValue({decimal: parseInt(newValue, 8).toString(10)});
+    
+        }
+        else{
+          this.form.patchValue({decimal: ""});
+        }
+        this.o = 0;
+      }
+  
+  
+
+  }
+
+  hexaChanged = function(oldValue,newValue){
+    this.h = this.h +1;
+    if(this.h == 1){
+      if(newValue != ""){
+        this.form.patchValue({decimal: parseInt(newValue, 16).toString(10)});
+      }
+      else{
+        this.form.patchValue({decimal: ""});
+      }
+      this.h = 0; 
+    }
+    
+  
+
+  }
+
+
+ onSubmit = function(user){
+   console.log(user);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import {HttpModule} from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
